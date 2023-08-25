53ec4b0034ae72609b66d49b606bd1ad0a482420
alexisloic21 <englebert.alexis@gmail.com>
2023-08-25 20:16:36 +0200

    stc: add static website

--- gen_static.sh
+++ gen_static.sh
@@ -9,14 +9,15 @@ END
 rm -rf static
 mkdir -p static
 cp -r public static/public
-cp CNAME static/CNAME
 
 cargo run &
 sleep 10 # XXX should be enough, lol
 
 for route in $ROUTES; do
 	mkdir -p static$route
-	fetch http://localhost:8000$route -o static$route/index.html
+	wget http://localhost:8000$route -o static$route/index.html
 done
+
+echo "Finished !"
 
 kill $(jobs -p)
