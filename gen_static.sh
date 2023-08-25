6eed425d09da5aad0850eabe5612c0b2563f87c4
alexisloic21 <englebert.alexis@gmail.com>
2023-08-25 20:30:08 +0200

    stc: upload website

--- gen_static.sh
+++ gen_static.sh
@@ -11,13 +11,11 @@ mkdir -p static
 cp -r public static/public
 
 cargo run &
-sleep 10 # XXX should be enough, lol
+sleep 5 # XXX should be enough, lol
 
 for route in $ROUTES; do
 	mkdir -p static$route
-	wget http://localhost:8000$route -o static$route/index.html
+	curl http://localhost:8000$route -o static$route/index.html
 done
-
-echo "Finished !"
 
 kill $(jobs -p)
